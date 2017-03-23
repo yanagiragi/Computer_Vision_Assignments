83337bc7a3e1b9afbceeda34aa9b7ab95b719126
yanagiragi <qaz90473@gmail.com>
2017-03-23 05:54:49 -0700

    Finish folding light Vector without normalized

--- CV_hw1/processor.cpp
+++ CV_hw1/processor.cpp
@@ -14,13 +14,10 @@ void processor::calculateNormals()
 	}*/
 	
 	Mat src = foldImgMatrix();
+	Mat light = foldLightVector();
 
-    cout << src.rows << "," <<  src.cols << endl;
-	for(int rowindex = 0; rowindex < src.rows; ++rowindex){
-		for(int colindex = 0; colindex < src.cols; ++colindex){
-			cout << static_cast<int>(src.at<uchar>(rowindex, colindex)) << " ";
-		}
-	}
+
+	cout << light << endl;
 	
 
 }
@@ -29,7 +26,6 @@ Mat processor::foldImgMatrix()
 {
 
 	Mat src = Mat(m_originalImg.size(), m_originalImg.begin()->second.total(), CV_8U);
-	cout << static_cast<int>(m_originalImg.size()) << endl;
 	
 	for(int i = 0; i < m_originalImg.size(); ++i){
 		for(int rowindex = 0; rowindex < m_originalImg[i+1].rows; ++rowindex){
@@ -38,6 +34,7 @@ Mat processor::foldImgMatrix()
 			}
 		}
 	}
+	cout << static_cast<int>(m_originalImg.size()) << endl;
 	/*
 	cout << src.rows << "," <<  src.cols << endl;
 	for(int rowindex = 0; rowindex < src.rows; ++rowindex){
@@ -52,5 +49,16 @@ Mat processor::foldImgMatrix()
 
 Mat processor::foldLightVector()
 {
-    // Mat light = Mat(m_originalLightSrc.size(), 3, CV_64F);
+	Mat light = Mat(m_originalLightSrc.size(), 3, CV_64F);
+	for(int i = 0; i < m_originalLightSrc.size(); ++i){
+		cout << static_cast<double>(m_originalLightSrc[i+1].x) << ", " 
+		<< static_cast<double>(m_originalLightSrc[i+1].y) << ", "
+		<< static_cast<double>(m_originalLightSrc[i+1].z) << endl;
+
+		light.at<double>(i,0) = static_cast<double>(m_originalLightSrc[i+1].x);
+		light.at<double>(i,1) = static_cast<double>(m_originalLightSrc[i+1].y);
+		light.at<double>(i,2) = static_cast<double>(m_originalLightSrc[i+1].z);
+	}
+
+	return light;
 }
